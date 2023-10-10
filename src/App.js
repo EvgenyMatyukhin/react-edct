09e865e547ed16ada67bf6defa8c51e116cac41c
Evgeny Matyukhin <mat,eugene@gmail.com>
2023-10-10 20:03:46 +0300

    new components in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <List/>
     </div>
   );
 }
+
+const Header = () => {
+  return (
+    <div className="App">
+      <div>Какая то новая функция</div>
+     </div>
+  );
+}
+
+const List = () => {
+  return (
+    <div className="App">
+        <ul>
+          <li>Проверка связи мать его за ногу</li>
+          <li>Проверка связи мать его за ногу</li>
+          <li>Проверка связи мать его за ногу</li>
+        </ul>
+      </div>
+  );
+}
+
 
 export default App;
