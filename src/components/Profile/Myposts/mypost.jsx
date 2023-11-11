3fb1d2d4da5df590b80792d66c88f2becc94aa9b
Evgeny Matyukhin <mat,eugene@gmail.com>
2023-11-11 10:07:42 +0300

    changes

--- src/components/Profile/Myposts/mypost.jsx
+++ src/components/Profile/Myposts/mypost.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import { addNewPostActionCreator, postChangeActionCreator } from '../../../redux/state';
 import style from './mypost.module.css'
 import Post from './Post/post';
 
 const MyPost = (props) => {
 
-    let posts = props.posts.map ( p => <Post massage={p.name} like={p.like}/>);
+    let posts = props.posts.map ( p => <Post massage={p.post} like={p.like}/>); // В структуру костанты Post вкладывам 
+                                                                                //содержимое из props
+
+    let newPost = React.createRef(); //Переменная, которая дает идентификатор для textarea
+
+    let addNewPost = () => {
+        props.dispatch(addNewPostActionCreator());
+    };
+
+    let postChange = () => {
+        let text = newPost.current.value; //забираем с помощью метода current содержимое (value) из textarea
+        props.dispatch(postChangeActionCreator(text)); //позволяет редактировать textarea из state
+    };
 
     return <div className={style.content}>
         <div className={style.post}>
             <p>Болталка</p>
             <div className={style.new_post}>
-                <textarea className={style.area} placeholder='Опубликовать новый пост'></textarea>
+                <textarea ref={newPost} onChange={postChange}
+                    value={props.newPost} className={style.area} placeholder='Введите новый пост'/>
                 <div className={style.button1}>
-                    <button>Опубликовать</button>
+                    <button onClick={addNewPost}>Опубликовать</button>
                 </div>
                 <div className={style.button2}>
                     <button>Очистить</button>
@@ -21,6 +35,7 @@ const MyPost = (props) => {
             {posts}
         </div>
     </div>
+
 };
 
 export default MyPost;
